ceb57070b3068f9ce18632115eb7eacb55ceb6aa
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-01-11 17:54:27 +0100

    nm IP_OPT

--- 0.6/consul-server/entrypoint.sh
+++ 0.6/consul-server/entrypoint.sh
@@ -2,8 +2,5 @@
 
 
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-if ! [ -z $IP ]; then
-  $IP_OPT="-advertise $IP"
-fi
 
-exec /bin/consul agent -server -config-dir=/config $IP_OPT $*
+exec /bin/consul agent -server -config-dir=/config -advertise $IP $*

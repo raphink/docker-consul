1b6e10da71cff45e74200e95c6763d77c2a36180
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-01-12 14:14:48 +0100

    Add BOOTSTRAP_EXPECT and DC env vars

--- 0.6/consul-server/entrypoint.sh
+++ 0.6/consul-server/entrypoint.sh
@@ -2,5 +2,9 @@
 
 
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+BOOTSTRAP_EXPECT=${$BOOSTRAP_EXPECT:-3}
+DC=${$DC:-dc1}
 
-exec /bin/consul agent -server -config-dir=/config -advertise $IP $*
+exec /bin/consul agent -server -config-dir=/config \
+  -advertise $IP -bootstrap-expect $BOOSTRAP_EXCEPT \
+  -dc $DC

0894263bc12b9c923ab16dce78e9efb7453959ee
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-01-12 14:23:22 +0100

    typo, no $

--- 0.6/consul-server/entrypoint.sh
+++ 0.6/consul-server/entrypoint.sh
@@ -2,8 +2,8 @@
 
 
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-BOOTSTRAP_EXPECT=${$BOOTSTRAP_EXPECT:-3}
-DC=${$DC:-dc1}
+BOOTSTRAP_EXPECT=${BOOTSTRAP_EXPECT:-3}
+DC=${DC:-dc1}
 
 exec /bin/consul agent -server -config-dir=/config \
   -advertise $IP -bootstrap-expect $BOOTSTRAP_EXCEPT \
